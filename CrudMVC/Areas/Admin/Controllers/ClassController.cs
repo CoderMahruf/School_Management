4cb5e81a88b75dc64fc20fc720543155d7b12699
ShihabK10 <shihab@encodersbd.com>
2026-03-08 12:46:35 +0600

    Update the Classes UI

--- CrudMVC/Areas/Admin/Controllers/ClassController.cs
+++ CrudMVC/Areas/Admin/Controllers/ClassController.cs
@@ -18,7 +18,13 @@ namespace CrudMVC.Areas.Admin.Controllers
         [HttpGet]
         public async Task<IActionResult> List()
         {
-            var classes = await _dbContext.Classes.ToListAsync();
+            var classes = await _dbContext.Classes
+                .Include(c => c.ClassSubjects)
+                .ThenInclude(cs => cs.Subject)
+                .ToListAsync();
+
+            ViewBag.Subjects = await _dbContext.Subjects.ToListAsync();
+
             return View(classes);
         }
 
@@ -31,6 +37,39 @@ namespace CrudMVC.Areas.Admin.Controllers
             };
 
             return View(vm);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> UpdateClass(int id, string name, List<int>? selectedSubjectIds)
+        {
+            var classInDb = await _dbContext.Classes
+                .Include(c => c.ClassSubjects)
+                .FirstOrDefaultAsync(c => c.Id == id);
+
+            if (classInDb == null)
+                return NotFound();
+
+            // update class name
+            classInDb.Name = name;
+
+            // remove old subjects
+            classInDb.ClassSubjects.Clear();
+
+            if (selectedSubjectIds != null)
+            {
+                foreach (var subjectId in selectedSubjectIds)
+                {
+                    classInDb.ClassSubjects.Add(new ClassSubject
+                    {
+                        ClassId = id,
+                        SubjectId = subjectId
+                    });
+                }
+            }
+
+            await _dbContext.SaveChangesAsync();
+
+            return RedirectToAction(nameof(List));
         }
 
         [HttpPost]
@@ -61,66 +100,6 @@ namespace CrudMVC.Areas.Admin.Controllers
             return RedirectToAction(nameof(List));
         }
 
-        [HttpGet]
-        public async Task<IActionResult> Edit(int id)
-        {
-            var classEntity = await _dbContext.Classes
-                .Include(c => c.ClassSubjects)
-                .FirstOrDefaultAsync(c => c.Id == id);
-
-            if (classEntity == null)
-                return NotFound();
-
-            var vm = new ClassEditVM
-            {
-                Id = classEntity.Id,
-                Name = classEntity.Name,
-                SelectedSubjectIds = classEntity.ClassSubjects
-                                        .Select(cs => cs.SubjectId)
-                                        .ToList(),
-                Subjects = await _dbContext.Subjects.ToListAsync()
-            };
-
-            return View(vm);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(ClassEditVM model)
-        {
-            if (!ModelState.IsValid)
-            {
-                model.Subjects = await _dbContext.Subjects.ToListAsync();
-                return View(model);
-            }
-
-            var classInDb = await _dbContext.Classes
-                .Include(c => c.ClassSubjects)
-                .FirstOrDefaultAsync(c => c.Id == model.Id);
-
-            if (classInDb == null)
-                return NotFound();
-
-            // Update name
-            classInDb.Name = model.Name;
-
-            // Remove old subjects
-            classInDb.ClassSubjects.Clear();
-
-            // Add new selected subjects
-            foreach (var subjectId in model.SelectedSubjectIds)
-            {
-                classInDb.ClassSubjects.Add(new ClassSubject
-                {
-                    ClassId = model.Id,
-                    SubjectId = subjectId
-                });
-            }
-
-            await _dbContext.SaveChangesAsync();
-
-            return RedirectToAction(nameof(List));
-        }
 
         [HttpPost]
         public async Task<IActionResult> Delete(int id)
